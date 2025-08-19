f26c8c66b072364ce9a967503e145ece6b458e8f
schoolemailme20-cpu <school.emailme20@gmail.com>
2025-08-18 23:07:07 -0700

    Initial commit – uploaded full website

---
+++ elite-consulting-website/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">Elite Academic Consulting</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Welcome to the future of academic consulting. Site under development.
+      </p>
+    </div>
+  );
+}
+
+export default App;
